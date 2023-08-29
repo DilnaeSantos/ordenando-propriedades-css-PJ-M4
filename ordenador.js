6cf5dc346b996f8f3a3918f4e9af574274c17248
DilnaeSantos <116980369+DilnaeSantos@users.noreply.github.com>
2023-08-29 13:38:09 -0300

    FEAT: imprementação da função pedirPropriedade

--- ordenador.js
+++ ordenador.js
@@ -4,3 +4,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+const propriedades = [];
+
+function pedirPropriedade() {
+    rl.question('Digite uma propriedade de CSS (ou "SAIR" para finalizar): ', (resposta) => {
+        if (resposta.toUpperCase() === 'SAIR') {
+            rl.close();
+        } else {
+            propriedades.push(resposta);
+            pedirPropriedade();
+        }
+    });
+}
